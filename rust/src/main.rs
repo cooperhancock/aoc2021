af1a7d5c202f3675d2c1005b16c227a89fa1ee9f
cooperhancock <23041745+dakotahlake@users.noreply.github.com>
2021-12-02 17:30:19 -0600

    part 2 rs done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let (h, d) = dive_part_1(&input_data());
     println!("part 1 solution: {}", h * d);
+    let (h, d, a) = dive_part_2(&input_data());
+    println!("part 1 solution: {}", h * d);
 }
 
 fn input_data() -> Vec<String> {
@@ -8,12 +10,13 @@ fn input_data() -> Vec<String> {
     return chonky_input.lines().map(|x| x.to_string()).collect();
 }
 
+// string -> (string, i32)
+fn split(s: &String) -> (&str, i32) {
+    let mut command = s.split_whitespace();
+    return (command.next().unwrap(), command.next().unwrap().parse::<i32>().unwrap());
+}
+
 fn dive_part_1(input: &Vec<String>) -> (i32, i32) {
-    // string -> (string, i32)
-    fn split(s: &String) -> (&str, i32) {
-        let mut command = s.split_whitespace();
-        return (command.next().unwrap(), command.next().unwrap().parse::<i32>().unwrap());
-    }
     let commands: Vec<(&str, i32)> = input.into_iter().map(split).collect();
     // string, i32, i32 -> i32, i32
     fn process((h, d): (i32, i32), c: (&str, i32)) -> (i32, i32){
@@ -25,4 +28,18 @@ fn dive_part_1(input: &Vec<String>) -> (i32, i32) {
         }
     }
     return commands.into_iter().fold((0, 0), process);
+}
+
+fn dive_part_2(input: &Vec<String>) -> (i32, i32, i32) {
+    let commands: Vec<(&str, i32)> = input.into_iter().map(split).collect();
+    // horizontal, depth, aim
+    fn process((h, d, a): (i32, i32, i32), c: (&str, i32)) -> (i32, i32, i32){
+        match c {
+            ("forward", x) => (h + x, d + a*x, a),
+            ("down", x) => (h, d, a + x),
+            ("up", x) => (h, d, a - x),
+            _ => panic!("oopsie woopsie the pattern doesnt match uwu")
+        }
+    }
+    return commands.into_iter().fold((0, 0, 0), process);
 }
