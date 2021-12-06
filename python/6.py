b4f84c98d95f515ada2dde3c70f8e4faff1b50b1
cooperhancock <23041745+dakotahlake@users.noreply.github.com>
2021-12-05 23:53:43 -0600

    day 6 made pretty

--- python/6.py
+++ python/6.py
@@ -4,34 +4,19 @@ with open(REPORTFILE, 'r') as f:
     report = f.readlines()
 report_len = len(report)
 fish = report[0][:-1].split(',')
-print(fish)
 fish = [int(x) for x in fish]
-print(fish)
 
-for i in range(80):
-    l = len(fish)
-    for f in range(l):
-        if fish[f] == 0:
-            fish.append(8)
-            fish[f] = 6
-            continue
-        fish[f] -= 1
+def fish_count(fish, days):
+    count = [0 for i in range(9)]
+    for f in fish:
+        count[f] += 1
+    for i in range(days):
+        l = len(count)
+        c0 = count[0]
+        for c in range(l-1):
+            count[c] = count[c+1]
+        count[6] += c0
+        count[8] = c0
+    return sum(count)
 
-print(f'fish count 80 days: {len(fish)}')
-
-fish = report[0][:-1].split(',')
-fish = [int(x) for x in fish]
-count = [0 for i in range(9)]
-for f in fish:
-    count[f] += 1
-
-for i in range(256):
-    l = len(count)
-    c0 = count[0]
-    for c in range(l-1):
-        count[c] = count[c+1]
-    count[6] += c0
-    count[8] = c0
-        
-
-print(f'total fish 256 days: {sum(count)}')
+print(f'80 days: {fish_count(fish, 80)}, 256 days: {fish_count(fish, 256)}')
